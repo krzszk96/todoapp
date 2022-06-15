9b8031e6cf939d7412461c0e2c75072aec150324
krzysztof <krzszk96@gmail.com>
2022-06-15 20:26:17 +0200

    display update delete from firebase

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 import { firebaseApp } from "./app_init.js"
 import { getAuth, onAuthStateChanged  } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js"
-import { getDatabase, ref, set, push, onChildAdded, onChildRemoved, remove } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-database.js"
+import { getDatabase, ref, set, push, onValue, update, remove } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-database.js"
 
 const auth = getAuth(firebaseApp)
+const db = getDatabase(firebaseApp)
 
 // On user login display user emial in navbar if not logged go to login page//
 var user_mail = document.getElementById('userMail')
@@ -10,6 +11,7 @@ var user_mail = document.getElementById('userMail')
 onAuthStateChanged(auth, (user) => {
   if (user) {
     user_mail.innerHTML = user.email
+    displayDataFromFirebase(user.uid)
   } else {
     window.location.href='index.html'
   }
@@ -35,34 +37,98 @@ add_task.addEventListener('click', () => {
         <button class="edit-btn" id="edit-btn">Edit</button>
       </div>`
   tasks_container.appendChild(task_html)
-  newtask.value = ''  
+   
+
+  // save to firebase database
+  let userId = auth.currentUser.uid
+  let dbRef = ref(db, 'users/' + userId + '/tasks')
+  let taskRef = push(dbRef)
+  set(taskRef, {
+    task: newtask.value,
+    state: 'pending'
+  })
+  
+  // reset input for new task
+  newtask.value = '' 
 })
+
+// retrieve and display elements from firebase
+function displayDataFromFirebase(uid){
+  let userId = auth.currentUser.uid
+  let dbRef = ref(db, 'users/' + uid + '/tasks')
+
+  onValue(dbRef, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const child = childSnapshot.val()
+      console.log(child.state);
+      const childkey = childSnapshot.key
+      let task_html = document.createElement('div')
+      task_html.classList.add('task-wrap')
+      task_html.setAttribute('id', childkey)
+      task_html.innerHTML =`
+      <div class="wrap">
+        <input type="checkbox" class="checkbox" id="check-state">
+        <label class="todo-item" id="task-label">${child.task}</label>
+      </div>
+      <div class="wrap">
+        <button class="delete-btn" id="delete-btn">Delete</button>
+        <button class="edit-btn" id="edit-btn">Edit</button>
+      </div>`
+      tasks_container.appendChild(task_html)
+      if (child.state == 'done') task_html.getElementsByClassName("checkbox")[0].toggleAttribute("checked");
+    });
+  }, {
+    onlyOnce: true
+  });
+}
 
 // delete item
 tasks_container.addEventListener('click', (e) => {
-  if(e.target.id == 'delete-btn') e.target.parentNode.parentNode.remove()  
+  
+  let element = e.target.parentNode.parentNode
+  if(e.target.id == 'delete-btn') {
+    element.remove()  
+  
+    // remove from firebase
+    let userId = auth.currentUser.uid
+    remove(ref(db, 'users/' + userId + '/tasks/' + element.id))
+  }
 })
 
 // edit item 
 tasks_container.addEventListener('click', (e) => {
   let element = e.target.parentNode.parentNode.querySelector('label')
+
+  let userId = auth.currentUser.uid //used for firebase
+  let elementid = e.target.parentNode.parentNode.id // used for firebase
+  let dbRef = ref(db, 'users/' + userId + '/tasks/' + elementid ) //path to firebase element
+
   if(e.target.id == 'edit-btn') {
-    if (element.contentEditable == 'true') {
-      element.contentEditable = 'false'
-    } else {
+    if (element.contentEditable != 'true') {
       element.contentEditable = 'true'
+    } else {
+      element.contentEditable = 'false'
+      // update firebase element      
+      update(dbRef,{ task: element.innerHTML })
     }
   }
 })
 
 // tick item as done
 tasks_container.addEventListener('click', (e) => {
-  let element = e.target.parentNode.parentNode.querySelector('label')
+  let checkbox = e.target.parentNode.parentNode.querySelector('input')
+
+  let userId = auth.currentUser.uid //used for firebase
+  let elementid = e.target.parentNode.parentNode.id // used for firebase
+  let dbRef = ref(db, 'users/' + userId + '/tasks/' + elementid ) //path to firebase element
+
+  if(checkbox.checked) console.log("yes");
+
   if(e.target.id == 'check-state') {
-    if (element.style.textDecoration === 'line-through') {
-      element.style.textDecoration = 'none'
+    if(!checkbox.checked) {
+      update(dbRef,{ state: 'pending' })
     } else {
-      element.style.textDecoration = 'line-through'
+      update(dbRef,{ state: 'done' })
     }
   }  
 })
